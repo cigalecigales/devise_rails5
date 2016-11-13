2f35ae567739d05252c39384cae092d5f2d1f848
cigalecigales <cigalecigales@gmail.com>
2016-11-13 12:44:21 +0900

    Ver1.0.0 Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+  root 'pages#index'
+
+  get 'pages/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
